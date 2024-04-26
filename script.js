95a099bbed5b66435884ed9698a1db67fe87c33b
felipe <130618161+fa9958189@users.noreply.github.com>
2024-04-26 10:42:17 -0300

    1 alteraçoao 26/04/2024
    1 alteraçoao 26/04/2024

---
+++ script.js
@@ -0,0 +1,8 @@
+function showText(element) {
+    element.getElementsByClassName("text-behind")[0].style.display = "block";
+  }
+  
+  function hideText(element) {
+    element.getElementsByClassName("text-behind")[0].style.display = "none";
+  }
+  
