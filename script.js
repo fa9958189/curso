9aea78d6fd5ba8898c8b1349334dea90c24cd801
felipe <130618161+fa9958189@users.noreply.github.com>
2024-10-23 10:49:25 -0300

    formulario criado
    formulario criado

--- script.js
+++
@@ -1,7 +0,0 @@
-function showText(element) {
-  element.getElementsByClassName("text-behind")[0].style.display = "block";
-}
-
-function hideText(element) {
-  element.getElementsByClassName("text-behind")[0].style.display = "none";
-}
